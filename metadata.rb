7df9981bf002753e79e9f33bcebb40c3b63ff89f
Thomas Boerger <tboerger@tbpro.de>
2013-10-22 13:09:54 +0200

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name "kvm"
+maintainer "Thomas Boerger"
+maintainer_email "tboerger@tbpro.de"
+license "Apache 2.0"
+description "Installs/Configures kvm"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version "0.0.1"
+depends "libvirt"
+recipe "kvm", "Installs/Configures kvm"
+
+supports "suse"
